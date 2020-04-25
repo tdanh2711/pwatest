cee824c1581bd2a5ec16b5643c145b557c4bb5d9
a <tdanh16028@cusc.ctu.edu.vn>
2020-04-26 01:10:13 +0700

    Update build;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService, Item } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pwatest';
+  items: Item[];
+
+  constructor(private apiService: ApiService) {
+  }
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.apiService.fetch().subscribe(
+      (data: Item[]) => {
+        console.log(data);
+        this.items = data;
+      }, (err) => {
+        console.log(err);
+      },
+    );
+  }
 }
